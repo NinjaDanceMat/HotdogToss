40d95ab5f4f265c03d93886ca75df88db5399d72
Conor Campbell <47574911+cc1005@users.noreply.github.com>
2025-01-25 11:51:38 +0000

    First Commit

---
+++ Assets/Scripts/BubbleScriptableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BubbleScriptableObject", menuName = "Scriptable Objects/BubbleScriptableObject")]
+public class BubbleScriptableObject : ScriptableObject
+{
+    
+}
